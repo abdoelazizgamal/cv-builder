a28272c8901c35338388f7ea85e3c275f7d1ba64
abdoelaziz gamal <abdoelaziz.gamal010@gmail.com>
2023-02-10 05:17:16 -1000

    edit pdf

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,45 +1,48 @@
 import { useEffect, useRef } from "react";
 import { ArrowDown } from "react-feather";
 import { useEditorContext } from "../../Context/EditorProvider";
-import { constraints } from "../../utils/captureScreen";
+
 import { colors } from "../../utils/constants";
 import Editor from "../Editor/Editor";
 import Resume from "../Resume/Resume";
 import styles from "./Body.module.css";
-
+import { useReactToPrint } from "react-to-print";
 const Body = () => {
   const { activeColor, setActiveColor } = useEditorContext();
   const resumeRef = useRef();
-  const exportPDF = async () => {
-    const el = document.getElementById("pdf-print");
-    el.scrollIntoView({ behavior: "smooth" });
-    document.body.classList.add("overflow-hidden");
-    try {
-      const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
-      const video = document.createElement("video");
-      video.addEventListener("loadedmetadata", () => {
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        video.play();
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-        stream.getVideoTracks()[0].stop();
-        const image = canvas.toDataURL("image/jpeg", 1.0);
-        var link = document.createElement("a");
-        link.href = image;
-        link.download = "cv.jpeg";
-        link.click();
+  // const exportPDF = async () => {
+  //   const el = document.getElementById("pdf-print");
+  //   el.scrollIntoView({ behavior: "smooth" });
+  //   document.body.classList.add("overflow-hidden");
+  //   try {
+  //     const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+  //     const video = document.createElement("video");
+  //     video.addEventListener("loadedmetadata", () => {
+  //       const canvas = document.createElement("canvas");
+  //       const ctx = canvas.getContext("2d");
+  //       canvas.width = video.videoWidth;
+  //       canvas.height = video.videoHeight;
+  //       video.play();
+  //       ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  //       stream.getVideoTracks()[0].stop();
+  //       const image = canvas.toDataURL("image/jpeg", 1.0);
+  //       var link = document.createElement("a");
+  //       link.href = image;
+  //       link.download = "cv.jpeg";
+  //       link.click();
 
-        return image;
-      });
-      video.srcObject = stream;
-    } catch (error) {
-      alert("Failed to capture screenshot!");
-    } finally {
-      document.body.classList.remove("overflow-hidden");
-    }
-  };
+  //       return image;
+  //     });
+  //     video.srcObject = stream;
+  //   } catch (error) {
+  //     alert("Failed to capture screenshot!");
+  //   } finally {
+  //     document.body.classList.remove("overflow-hidden");
+  //   }
+  // };
+  const exportPDF = useReactToPrint({
+    content: () => resumeRef.current,
+  });
   useEffect(() => {
     document.documentElement.style.setProperty("--first-color", activeColor[0]);
     document.documentElement.style.setProperty(
